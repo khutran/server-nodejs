23800a8ca79ff9b5fd8300194d518067ee567f85
khutran <33168978+khutran@users.noreply.github.com>
2018-05-28 14:50:11 +0700

    Update run-httpd.sh

--- docker/httpd/run-httpd.sh
+++ docker/httpd/run-httpd.sh
@@ -5,4 +5,5 @@
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
 
-exec /usr/sbin/apachectl -DFOREGROUND && /usr/sbin/php-fpm --nodaemonize
+exec /usr/sbin/php-fpm --nodaemonize
+exec /usr/sbin/apachectl -DFOREGROUND
