2b3616f98fc790475d47420dccf727344a3aafcb
Your Name <you@example.com>
2018-06-29 15:28:29 +0700

    udpate

--- docker/server/run-httpd.sh
+++ docker/server/run-httpd.sh
@@ -4,8 +4,5 @@
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
 rm -rf /run/nginx*
-rm -rf /root/.forever/pids/*
-rm -rf /root/.forever/sock/*
 
-exec nginx &
-exec forever start /var/www/web/ecosystem/*.json
+exec nginx
