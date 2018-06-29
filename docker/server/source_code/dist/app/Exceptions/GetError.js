9df53dbfc2ecf7517cb42b6ba70c1d620be667a3
Your Name <you@example.com>
2018-06-29 15:37:20 +0700

    update

--- docker/server/source_code/dist/app/Exceptions/GetError.js
+++
@@ -1,7 +0,0 @@
-export default class getError {
-
-    constructor(e = "", code = 500) {
-        let error = new Error(e);
-        return { message: error['message'], error_code: code };
-    }
-}
