e2ed4acaedc9e18745b2b205e78d0acee1ed6ba3
Your Name <you@example.com>
2018-06-29 14:45:05 +0700

    update

--- docker/server/run-httpd.sh
+++ docker/server/run-httpd.sh
@@ -4,4 +4,8 @@
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
 rm -rf /run/nginx*
-exec nginx
+rm -rf /root/.forever/pids/*
+rm -rf /root/.forever/sock/*
+
+exec nginx &
+exec forever start /var/www/web/ecosystem/*.json
