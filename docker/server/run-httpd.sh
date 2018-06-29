1119ba63f12ae60f668951aad49f0d4ab5f3aedb
Your Name <you@example.com>
2018-06-29 11:14:19 +0700

    update

--- docker/server/run-httpd.sh
+++ docker/server/run-httpd.sh
@@ -3,10 +3,5 @@
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
-rm -rf /run/httpd/* /tmp/httpd*
 rm -rf /run/nginx*
-
-exec /usr/sbin/apachectl -DFOREGROUND &
 exec nginx &
-exec /usr/sbin/php-fpm --nodaemonize &
-exec supervisord -c /etc/supervisord.conf
