d975fbefb9ae8d447517891d33e6bba1d4adca1c
Your Name <you@example.com>
2018-07-06 10:33:52 +0700

    update

--- docker/server/run-httpd.sh
+++ docker/server/run-httpd.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+name="$(cat /home/name.txt)"
+path="/var/www/web"
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
+
 rm -rf /run/nginx*
-# rm -rf /root/.forever/pids/*
-# rm -rf /root/.forever/sock/*
+rm -rf /root/.forever/pids/*
+rm -rf /root/.forever/sock/*
+
+if [ ! -d "$path/$name" ]; then
+  if [ -d "$path/source_code" ]; then
+    mkdir $path/$name
+    mv $path/source_code $path/$name/workspace
+  fi
+else 
+  echo "$path/$name"
+fi
+
+cd $path/$name/workspace
+
+yarn install
+yarn build 
+
+forever start "/var/www/config/ecosystem/"$name".json"
 
 exec nginx
